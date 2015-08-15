27a3bb2c64aeafcdba460e55cc884be74100a8e5
Danielle Blank <dgdblank@gmail.com>
2015-08-15 10:56:42 -0700

    (fix) missing quotations and npm install

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 // configure server with middleware and routing
-require(./middleware.js)(app, express);
+require("./middleware.js")(app, express);
 
 var port = 3000;
 app.listen(port, function(){
