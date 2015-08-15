26351884151a8dca185c41764287947e2dd62104
Danielle Blank <dgdblank@gmail.com>
2015-08-15 10:35:14 -0700

    Server and route configuration

---
+++ server/server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+// configure server with middleware and routing
+require(./middleware.js)(app, express);
+
+var port = 3000;
+app.listen(port, function(){
+	console.log("Listening on " + port);
+})
+
+module.exports = app;
