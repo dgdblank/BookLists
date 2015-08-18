37f6fd72431800bc7aec01e2d4bd590bb007b15d
Danielle Blank <dgdblank@gmail.com>
2015-08-17 23:17:59 -0700

    (server) Add book to database and to join table with list

--- server/lists/listController.js
+++ server/lists/listController.js
@@ -22,10 +22,39 @@ module.exports = {
   addBook: function(req, res){
     console.log("got to add book");
     console.log('list', req.list);
-    console.log('book', req.body);
-    // add the book to book table
+    console.log('book', req.body.volumeInfo);
+    var book = req.body.volumeInfo;
+
+    Book.forge({
+      title: book.title, 
+      author: book.authors[0]
+    })
+    .fetch()
+    .then(function (foundBook){
+      console.log('book in database?', foundBook);
+      if(!foundBook){
+        Book.forge({
+          title: book.title, 
+          author: book.authors[0],
+          genre: book.categories[0],
+          pages: book.pageCount,
+          thumbnail: book.imageLinks.thumbnail
+        })
+        .save()
+        .then(function (newBook){
+          newBook.related('lists').attach([req.list.id]);
+          res.sendStatus(201);
+        })
+      } else {
+        foundBook.related('lists').attach([req.list.id]);
+        res.sendStatus(201);
+      }
+    })
+    .catch(function (error){
+      console.log(error);
+    });
     // add book and list to join table
     // return 200 code
   }
 
-};
+}
