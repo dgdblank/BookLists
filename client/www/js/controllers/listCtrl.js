ce5407bac45b764cd9b1d18f190ba99b20aae466
Danielle Blank <dgdblank@gmail.com>
2015-08-18 14:55:54 -0700

    Show list name on list view

--- client/www/js/controllers/listCtrl.js
+++ client/www/js/controllers/listCtrl.js
@@ -1,22 +1,22 @@
 angular.module('starter.list', [])
 
 .controller('ListCtrl', function ($scope, $stateParams, Books, Lists){
-  $scope.books = [];
-  $scope.listId = $stateParams.listId;
-
+  
   $scope.$on('$ionicView.enter', function(e){
-    if(!$scope.list){
-      $scope.list = Lists.getName();
-    }
-  })
-  console.log('scope.list', $scope.list);
+    $scope.books = [];
+    $scope.listId = $stateParams.listId;
+    $scope.list = Lists.getName();
+    console.log('scope.list', $scope.list);
 
-  $scope.getBooks = function(){
-    Books.getBooks($scope.listId)
-      .then(function (data){
-        $scope.books = data.books;
-      });
-  };
+    $scope.getBooks = function(){
+      Books.getBooks($scope.listId)
+        .then(function (data){
+          $scope.books = data.books;
+        });
+    };
 
-  $scope.getBooks();
+    $scope.getBooks();
+
+  });
+
 });
